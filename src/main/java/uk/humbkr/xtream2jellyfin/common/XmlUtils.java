692c4ad81ea6480f3defae7c502aa9aebb82d50a
Mickaël Salmon <mickael@humbkr.uk>
2026-01-18 18:29:12 +0100

    fix: dont write null values in NFOs

--- src/main/java/uk/humbkr/xtream2jellyfin/common/XmlUtils.java
+++ src/main/java/uk/humbkr/xtream2jellyfin/common/XmlUtils.java
@@ -1,5 +1,6 @@
 package uk.humbkr.xtream2jellyfin.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.Getter;
@@ -12,6 +13,7 @@ public class XmlUtils {
     private static XmlMapper initializeXmlMapper() {
         XmlMapper xmlMapper = new XmlMapper();
         xmlMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        xmlMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
         return xmlMapper;
     }
 
